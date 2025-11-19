b67b2eee5c93f3eb97b651416a428cf1b54408b7
keinyhm <keinyhm@gmail.com>
2025-11-19 13:57:03 +0100

    creando entidad profesor

---
+++ app/models/profesor.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Profesor:
+    id: int
+    nombre: str
+    correo: str
+    telefono: str
+    titulo: str
+    id_departamento: int #clave foránea de la entidad departamento
+    jefe_dtp: bool #es jefe o lider  de departamento o no
