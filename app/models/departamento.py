abd670e1277025b718bb0eadcc7ca67a0501bce5
Orphereum <josemcr28012001@gmail.com>
2025-11-19 14:17:05 +0100

    modificación departamento

--- app/models/departamento.py
+++ app/models/departamento.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 class Departamento:
     id_departamento: int 
     nombre: str
-    facultad: str 
     id_facultad: int #clave foranea de facultad
+    facultad: str 
