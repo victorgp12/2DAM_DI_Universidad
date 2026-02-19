bccb7fcf877c30f23c2329e1799760c022af52c3
AdriTg00 <adrian2000tg@gmail.com>
2026-02-19 12:13:55 +0100

    Cambios en data

--- app/data/db.py
+++ app/data/db.py
@@ -1,26 +1,22 @@
 import sqlite3
 import os
-
-
-# RUTA A LA BASE DE DATOS (app/universidad.db)
+import sys
 
 def _get_db_path():
-    base_dir = os.path.dirname(os.path.abspath(__file__))  
-    # __file__ ya está dentro de app/
-    db_path = os.path.join(base_dir, "universidad.db")
-    return db_path
-
-
-# CONEXIÓN
+    if getattr(sys, 'frozen', False):  # Es EXE de PyInstaller
+        # PyInstaller extrae archivos a _MEIPASS
+        base_dir = sys._MEIPASS
+    else:
+        # Desarrollo: directorio del proyecto (padre del actual)
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    return os.path.join(base_dir, "universidad.db")
 
 def get_connection():
     db_path = _get_db_path()
     print("Usando BD:", db_path)
-
-    # 🚨 Si no existe, NO queremos que la cree
     if not os.path.exists(db_path):
-        raise FileNotFoundError(f"No existe la base de datos en: {db_path}")
-
+        raise FileNotFoundError(f"BD no encontrada: {db_path}")
     conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row 
     return conn
