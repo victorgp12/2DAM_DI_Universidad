d975f26ac2d9fd8c1ea7122f5056fa21958f2021
victorgp12 <victorvp200412@gmail.com>
2026-02-25 12:00:07 +0100

    update grupoInv_repository

--- app/repository/grupoInv_repo.py
+++ app/repository/grupoInv_repo.py
@@ -3,7 +3,7 @@ from app.models.grupoInv import GrupoInvestigacion
 
 class GrupoInvRepository:
     
-    def cargar_lista_gruposInv(self):
+    def cargar_lista_grupos_inv(self):
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT g.id_grupo, g.nombre FROM grupo_investigacion g")
@@ -19,7 +19,7 @@ class GrupoInvRepository:
         conn.close()
         return [GrupoInvestigacion(id=row[0], id_grupoInv=row[1], nombre=row[2], descripcion=row[3], fecha_creacion=row[4]) for row in rows]
     
-    def delete_grupoInv(self, id):
+    def delete_grupo_inv(self, id):
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("DELETE FROM grupo_investigacion WHERE id = ?", (id,))
@@ -37,7 +37,7 @@ class GrupoInvRepository:
         conn.commit()
         conn.close()
 
-    def get_grupoInv_by_id(self, id):
+    def get_grupo_inv_by_id(self, id):
         conn = get_connection()
         cursor = conn.cursor()
 
