d3b9a9fb7b4ade3b1786648bba17be5fb9642fdf
victorgp12 <victorvp200412@gmail.com>
2026-02-25 12:15:39 +0100

    actualizacion grupoInv_service

--- app/service/grupoInv_service.py
+++ app/service/grupoInv_service.py
@@ -3,8 +3,8 @@ from app.repository.grupoInv_repo import GrupoInvRepository
 
 class GrupoInvService:
     
-    def __init__(self, grupoInv_repo):
-        self.grupoInv_repo = grupoInv_repo
+    def __init__(self, grupo_inv_repo):
+        self.grupo_inv_repo = grupo_inv_repo
     
-    def cargar_lista_gruposInv(self):
-        return self.grupoInv_repo.cargar_lista_gruposInv()
+    def cargar_lista_grupos_inv(self):
+        return self.grupo_inv_repo.cargar_lista_gruposInv()
